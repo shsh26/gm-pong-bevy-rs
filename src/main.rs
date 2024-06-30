9b63cd562d5e74fb609887f49ff5998c4cf32f07
shsh26 <sagong126@gmail.com>
2024-07-01 00:08:11 +0900

    :sparkles: feat: add ai and player paddles

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use bevy::prelude::*;
 use bevy::sprite::MaterialMesh2dBundle;
 use bevy::math::bounding::{Aabb2d, BoundingCircle, BoundingVolume, IntersectsVolume};
+use bevy::prelude::KeyCode::{ArrowDown, ArrowUp};
 
 const BALL_SIZE: f32 = 5.0;
 
-const PADDLE_SPEED: f32 = 1.0;
+const PADDLE_SPEED: f32 = 5.0;
 const PADDLE_WIDTH: f32 = 10.0;
 const PADDLE_HEIGHT: f32 = 50.0;
+
+const GUTTER_HEIGHT: f32 = 20.0;
 
 #[derive(Component)]
 struct Position(Vec2);
@@ -31,6 +34,29 @@ enum Collision {
     Bottom,
 }
 
+#[derive(Component)]
+struct Gutter;
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Component)]
+struct Ai;
+
+enum Scorer {
+    Player,
+    Ai,
+}
+
+#[derive(Event)]
+struct Scored(Scorer);
+
+#[derive(Resource, Default)]
+struct Score {
+    player: u32,
+    ai: u32,
+}
+
 #[derive(Bundle)]
 struct BallBundle {
     ball: Ball,
@@ -44,6 +70,13 @@ struct PaddleBundle {
     paddle: Paddle,
     shape: Shape,
     velocity: Velocity,
+    position: Position,
+}
+
+#[derive(Bundle)]
+struct GutterBundle {
+    gutter: Gutter,
+    shape: Shape,
     position: Position,
 }
 
@@ -64,6 +97,16 @@ impl PaddleBundle {
             paddle: Paddle,
             shape: Shape(Vec2::new(PADDLE_WIDTH, PADDLE_HEIGHT)),
             velocity: Velocity(Vec2::new(0.0, 0.0)),
+            position: Position(Vec2::new(x, y)),
+        }
+    }
+}
+
+impl GutterBundle {
+    fn new(x: f32, y: f32) -> Self {
+        Self {
+            gutter: Gutter,
+            shape: Shape(Vec2::new(0.0, GUTTER_HEIGHT)),
             position: Position(Vec2::new(x, y)),
         }
     }
@@ -106,23 +149,88 @@ fn spawn_ball(mut commands: Commands,
 
 fn spawn_paddle(mut commands: Commands,
                 mut meshs: ResMut<Assets<Mesh>>,
-                mut materials: ResMut<Assets<ColorMaterial>>) {
-
-    let shape = Mesh::from(Rectangle::new(PADDLE_WIDTH, PADDLE_HEIGHT));
-    let material = ColorMaterial::from(Color::GREEN);
-
-    let mesh_handle = meshs.add(shape);
-    let material_handle = materials.add(material);
+                mut materials: ResMut<Assets<ColorMaterial>>,
+                window: Query<&Window>
+) {
 
     println!("Spawning paddle");
+
+    if let Ok(window) = window.get_single() {
+        let window_width = window.resolution.width();
+        let padding = 50.;
+        let right_paddle_x = window_width / 2. - padding;
+        let left_paddle_x = -window_width / 2. + padding;
+
+        let mesh = Mesh::from(Rectangle::new(
+            PADDLE_WIDTH,
+            PADDLE_HEIGHT,
+        ));
+
+        let mesh_handle = meshs.add(mesh);
+
+        commands.spawn((
+            Player,
+            PaddleBundle::new(right_paddle_x, 0.),
+            MaterialMesh2dBundle {
+                mesh: mesh_handle.clone().into(),
+                material: materials.add(ColorMaterial::from(Color::GREEN)),
+                ..Default::default()
+            }
+        ));
+
     commands.spawn((
-        PaddleBundle::new(20., -25.),
+        Ai,
+        PaddleBundle::new(left_paddle_x, 0.),
         MaterialMesh2dBundle {
             mesh: mesh_handle.into(),
-            material: material_handle,
+            material: materials.add(ColorMaterial::from(Color::BLUE)),
             ..Default::default()
     }));
+    }
 }
+
+fn spawn_gutter(
+    mut commands: Commands,
+    mut meshs: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    window: Query<&Window>,
+) {
+    if let Ok(window) = window.get_single() {
+        let window_width = window.resolution.width();
+        let window_height = window.resolution.height();
+        let padding = 50.;
+
+        let top_gutter = GutterBundle::new(0., window_height / 2. - GUTTER_HEIGHT / 2.);
+        let bottom_gutter = GutterBundle::new(0., -window_height / 2. + GUTTER_HEIGHT / 2.);
+
+        let mesh = Mesh::from(Rectangle::from_size(
+            top_gutter.shape.0
+        ));
+        let material = ColorMaterial::from(Color::BLACK);
+
+        let mesh_handle = meshs.add(mesh);
+        let material_handle = materials.add(material);
+
+        commands.spawn((
+            top_gutter,
+            MaterialMesh2dBundle {
+                mesh: mesh_handle.clone().into(),
+                material: material_handle.clone(),
+                ..Default::default()
+            }
+        ));
+
+        commands.spawn((
+            bottom_gutter,
+            MaterialMesh2dBundle {
+                mesh: mesh_handle.into(),
+                material: material_handle.clone(),
+                ..Default::default()
+            }
+        ));
+    }
+}
+
 
 fn move_ball(mut ball: Query<(&mut Position, &Velocity), With<Ball>>) {
     if let Ok((mut position, velocity)) = ball.get_single_mut() {
@@ -187,6 +295,38 @@ fn handle_collisions(
                         ball_velocity.0.y *= -1.;
                     }
                 }
+            }
+        }
+    }
+}
+
+fn handle_player_input(
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mut paddle: Query<&mut Velocity, With<Player>>,
+) {
+    if let Ok(mut velocity) = paddle.get_single_mut() {
+        if keyboard_input.pressed(ArrowUp) {
+            velocity.0.y = 1.;
+        } else if keyboard_input.pressed(ArrowDown) {
+            velocity.0.y = -1.;
+        } else {
+            velocity.0.y = 0.;
+        }
+    }
+}
+
+fn move_paddle(
+    mut paddle: Query<(&mut Position, &Velocity), With<Paddle>>,
+    window: Query<&Window>
+) {
+    if let Ok(window) = window.get_single() {
+        let window_height = window.resolution.height();
+        let max_y = window_height / 2. - GUTTER_HEIGHT - PADDLE_HEIGHT / 2.;
+
+        for (mut position, velocity) in &mut paddle {
+            let new_position = position.0 + velocity.0 * PADDLE_SPEED;
+            if new_position.y.abs() < max_y {
+                position.0 = new_position;
             }
         }
     }
